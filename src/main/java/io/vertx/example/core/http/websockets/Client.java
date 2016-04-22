273364cf3912c1bd681d163abbe26a28b5702e4c
quyenlm <quyenlm.vn@gmail.com>
2016-04-22 15:30:02 +0700

    gf

--- src/main/java/io/vertx/example/core/http/websockets/Client.java
+++ src/main/java/io/vertx/example/core/http/websockets/Client.java
@@ -1,6 +1,7 @@
 package io.vertx.example.core.http.websockets;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpClient;
 import io.vertx.example.util.Runner;
@@ -9,22 +10,31 @@ import io.vertx.example.util.Runner;
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 public class Client extends AbstractVerticle {
+	String host = "gettingstart.herokuapp.com";
+	String uri = "/";
+	int port = 8080;
+	
+	// Convenience method so you can run it in your IDE
+	public static void main(String[] args) {
+		Runner.runExample(Client.class);
+	}
 
-  // Convenience method so you can run it in your IDE
-  public static void main(String[] args) {
-    Runner.runExample(Client.class);
-  }
+	@Override
+	public void start() throws Exception {
+		if (vertx == null) {
+			System.out.println("init vertx context...");
+			vertx = Vertx.vertx();
+		}
+		
+		HttpClient client = vertx.createHttpClient();
 
-  @Override
-  public void start() throws Exception {
-    HttpClient client = vertx.createHttpClient();
-
-    client.websocket(8080, "localhost", "/some-uri", websocket -> {
-      websocket.handler(data -> {
-        System.out.println("Received data " + data.toString("ISO-8859-1"));
-        client.close();
-      });
-      websocket.writeBinaryMessage(Buffer.buffer("Hello world"));
-    });
-  }
+		client.websocket(port, host, uri,
+				websocket -> {
+					websocket.handler(data -> {
+						System.out.println("Received data " + data.toString("ISO-8859-1"));
+						client.close();
+					});
+					websocket.writeBinaryMessage(Buffer.buffer("Hello world"));
+				});
+	}
 }
